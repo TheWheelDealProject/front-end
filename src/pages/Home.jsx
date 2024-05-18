0cd87ffc76bba63b01dfb609b469b6475dc39f3d
Yassin-010 <YassinSE898@gmail.com>
2024-05-18 09:29:52 +0000

    car in user view done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import HeroSlider from "../components/UI/HeroSlider";
 import Helmet from "../components/Helmet/Helmet";
@@ -7,14 +7,27 @@ import { Container, Row, Col } from "reactstrap";
 import FindCarForm from "../components/UI/FindCarForm";
 import AboutSection from "../components/UI/AboutSection";
 import ServicesList from "../components/UI/ServicesList";
-import carData from "../assets/data/carData";
+// import carData from "../assets/data/carData";
 import CarItem from "../components/UI/CarItem";
 import BecomeDriverSection from "../components/UI/BecomeDriverSection";
 import Testimonial from "../components/UI/Testimonial";
 
 import BlogList from "../components/UI/BlogList";
+import axios from "axios";
+
 
 const Home = () => {
+
+  const [carData, setCarData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/getAllCars")
+      .then((response) =>{
+        setCarData(response.data.cars)
+      } )
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <Helmet title="Home">
       {/* ============= hero section =========== */}
