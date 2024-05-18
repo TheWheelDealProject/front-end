81920002667f215239ac3d44bc4305bf22c0d4ae
MohammadAlfarwan <alfrwan58@gmail.com>
2024-05-18 12:29:29 +0300

    Car Details Page is completed

--- src/pages/Admin/AddCar.jsx
+++ src/pages/Admin/AddCar.jsx
@@ -9,7 +9,7 @@ function AddCar() {
             <Container>
                 <Row>
                     <Col>
-                        < AddCarForm />
+                        < AddCarForm  />
                     </Col>
                 </Row>
             </Container>
